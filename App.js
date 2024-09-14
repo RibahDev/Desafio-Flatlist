0825ea6b5809832fa2c246ae85fafcdfeb4eda66
Ribeiro <joao.ribeiro17@outlook.com>
2024-09-13 22:24:55 -0300

    Iniciando app de desafio em react-native

---
+++ App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native'
+import DetalhePessoaScreen from './components/DetalhePessoaScreen';
+import ListaPessoasScreen from './components/ListaPessoasScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='ListaPessoa'>
+        <Stack.Screen name="ListaPessoa" component={ListaPessoasScreen} />
+        <Stack.Screen name="DetalhePessoa" component={DetalhePessoaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ data/Pessoas.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: '1',
+        urlImagem: '',
+       name: 'José André Ronald',
+       email: 'joseron@hotmail.com'
+    },
+    {
+        id: '2',
+        urlImagem: '',
+       name: 'Marcio Fernando de Souza',
+       email: 'marcinhofer@hotmail.com'
+    },
+    {
+        id: '3',
+        urlImagem: '',
+       name: 'Aline Cristina da Silva',
+       email: 'alinecris@hotmail.com'
+    },
+    {
+        id: '4',
+        urlImagem: '',
+       name: 'Thiago Maurélio Junior',
+       email: 'thiagomau@hotmail.com'
+    },
+    {
+        id: '5',
+        urlImagem: '',
+       name: 'Amanda Roberta dos Santos',
+       email: 'amanda12@hotmail.com'
+    },
+
+]
